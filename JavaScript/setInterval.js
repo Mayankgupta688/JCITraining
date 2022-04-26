5a1c6073397a254a04622f496ef1db5ef4980759
MayankGupta <mayank.gupta.6.88@gmail.com>
2022-04-26 13:22:48 +0530

    Session One

---
+++ JavaScript/setInterval.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+    console.log("Hello All");
+}, 5000)
+
+setTimeout(function() {
+    console.log("Welcome to Technofunnel");
+}, 3000)
+
+console.log("Welcome to Session");
+
+// JavaScript is single thread execution model = Call Stack
+// JavaScript is Multi Threaded Architecture - Web Api, Callback Query
